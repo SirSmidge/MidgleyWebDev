3e11d0c17ca55d37a5dbaa43b1e00d7d69d2b578
SirSmidge <stevenmwebdev@gmail.com>
2019-09-02 13:03:41 -0600

    I have the nav animation shorted. I am working on getting the images into the cards. need to leave for now.

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
-let navStyle=document.getElementById('sideNav');
-let bodyStyle=document.getElementById('contentBody');
+let navStyle = document.getElementById('sideNav');
+let bodyStyle = document.getElementById('contentBody');
+let burgerStyle = document.getElementById('burger'); 
 
 openBtn = () => {
   navStyle.style.width = '150px';
-  bodyStyle.style.marginTop = '75px';
+  bodyStyle.style.marginTop = '105px';
+  burgerStyle.style.display = 'none';
 }
 
 closeBtn = () => {
   navStyle.style.width = '0';
   bodyStyle.style.marginTop = '0';
+  burgerStyle.style.display = 'list-item';
 }
