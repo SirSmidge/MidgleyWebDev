ef75fb43bd67bc120e9c955a99bc3625835789e6
SirSmidge <stevenmwebdev@gmail.com>
2019-09-02 23:00:30 -0600

    made great progress today.

--- public/index.js
+++ public/index.js
@@ -2,13 +2,13 @@ let navStyle = document.getElementById('sideNav');
 let bodyStyle = document.getElementById('contentBody');
 let burgerStyle = document.getElementById('burger'); 
 
-openBtn = () => {
-  navStyle.style.width = '95vw';
-  bodyStyle.style.marginTop = '14em';
+openBtn = () =>  {
+  navStyle.style.width = '98vw';
+  bodyStyle.style.marginTop = '15em';
   burgerStyle.style.display = 'none';
 }
 
-closeBtn = () => {
+closeBtn = () =>  {
   navStyle.style.width = '0';
   bodyStyle.style.marginTop = '0';
   burgerStyle.style.display = 'list-item';
